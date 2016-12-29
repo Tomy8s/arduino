e6a5aae5609baa5cb4f9cf12b533afe5bd7f501f
Tomy8s <tomy8s@hotmail.com>
2016-12-29 05:19:43 +0000

    encapsulated dots/dashes and letters into functions

--- SOS/SOS.ino
+++ SOS/SOS.ino
@@ -4,41 +4,35 @@ void setup() {
   pinMode(led, OUTPUT);
 }
 
+int dot() {
+  digitalWrite(led, HIGH);
+  delay(100);
+  digitalWrite(led, LOW);
+  delay(150);
+}
+
+int dash() {
+  digitalWrite(led, HIGH);
+  delay(700);
+  digitalWrite(led, LOW);
+  delay(150);
+}
+
+int s() {
+  dot();
+  dot();
+  dot();
+}
+
+int o() {
+  dash();
+  dash();
+  dash();
+}
+
 void loop() {
-  digitalWrite(led, HIGH);
-  delay(100);
-  digitalWrite(led, LOW);
-  delay(100);
-  digitalWrite(led, HIGH);
-  delay(100);
-  digitalWrite(led, LOW);
-  delay(100);
-  digitalWrite(led, HIGH);
-  delay(100);
-  digitalWrite(led, LOW);
-  delay(100);
-  digitalWrite(led, HIGH);
-  delay(250);
-  digitalWrite(led, LOW);
-  delay(100);
-  digitalWrite(led, HIGH);
-  delay(250);
-  digitalWrite(led, LOW);
-  delay(100);
-  digitalWrite(led, HIGH);
-  delay(250);
-  digitalWrite(led, LOW);
-  delay(100);
-  digitalWrite(led, HIGH);
-  delay(100);
-  digitalWrite(led, LOW);
-  delay(100);
-  digitalWrite(led, HIGH);
-  delay(100);
-  digitalWrite(led, LOW);
-  delay(100);
-  digitalWrite(led, HIGH);
-  delay(100);
-  digitalWrite(led, LOW);
-  delay(1200);
+  s();
+  o();
+  s();
+  delay(1000);
 }
