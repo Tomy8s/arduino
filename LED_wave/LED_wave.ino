c71c51a3362f78968d216baf0fb148304329139f
Tomy8s <tomy8s@hotmail.com>
2016-12-30 22:17:30 +0000

    a new program for a Mexican wave of LEDs

---
+++ LED_wave/LED_wave.ino
@@ -0,0 +1,24 @@
+int led = 1; //the pin number of lit LED
+int dir = 1; //the direction of the wave (1 increases, -1 decreases)
+int dur = 25; //the time in ms each LED is alight
+
+void setup() {
+  for(int led = 1; led < 14; led += 1) {
+    pinMode(led, OUTPUT);
+  }
+}
+
+void loop() {  
+  digitalWrite(led, HIGH);
+  delay(dur);
+  digitalWrite(led, LOW);
+  
+  led += dir;
+  
+  if (led == 13) {
+    dir = -1;
+  }
+  if (led == 1) {
+    dir = 1;
+  }
+}
---
+++ LED_wave/README.md
@@ -0,0 +1,5 @@
+# Wave
+
+This program uses 13 LEDs connected to each of the output pins of an arduino UNO (I've used 220 ohm resistors). It creates a rippling wave effect by flashing an LED for a short time (given in ms by the "dur" variable) and then the next.  
+
+Watch the video on [YouTube](https://www.youtube.com/watch?v=pEe-fRxjB7c).
