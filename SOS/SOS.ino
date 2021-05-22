804faf036072b6d53b46fc52e07b5068d451127f
Thomas Yates <Thomas.Yates@bglgroup.co.uk>
2021-05-22 10:50:27 +0100

    use C++ formating conventions

--- SOS/SOS.ino
+++ SOS/SOS.ino
@@ -1,38 +1,44 @@
 int led = 3;
 
-void setup() {
-  pinMode(led, OUTPUT);
+void setup()
+{
+    pinMode(led, OUTPUT);
 }
 
-int dot() {
-  digitalWrite(led, HIGH);
-  delay(100);
-  digitalWrite(led, LOW);
-  delay(150);
+int dot()
+{
+    digitalWrite(led, HIGH);
+    delay(100);
+    digitalWrite(led, LOW);
+    delay(150);
 }
 
-int dash() {
-  digitalWrite(led, HIGH);
-  delay(700);
-  digitalWrite(led, LOW);
-  delay(150);
+int dash()
+{
+    digitalWrite(led, HIGH);
+    delay(700);
+    digitalWrite(led, LOW);
+    delay(150);
 }
 
-int s() {
-  dot();
-  dot();
-  dot();
+int s()
+{
+    dot();
+    dot();
+    dot();
 }
 
-int o() {
-  dash();
-  dash();
-  dash();
+int o()
+{
+    dash();
+    dash();
+    dash();
 }
 
-void loop() {
-  s();
-  o();
-  s();
-  delay(1000);
+void loop()
+{
+    s();
+    o();
+    s();
+    delay(1000);
 }
--- blink_500/blink_500.ino
+++ blink_500/blink_500.ino
@@ -1,10 +1,12 @@
-void setup() {
-  pinMode(13, OUTPUT);
+void setup()
+{
+    pinMode(13, OUTPUT);
 }
 
-void loop() {
-  digitalWrite(13, HIGH);
-  delay(500);
-  digitalWrite(13, LOW);
-  delay(500);
+void loop()
+{
+    digitalWrite(13, HIGH);
+    delay(500);
+    digitalWrite(13, LOW);
+    delay(500);
 }
